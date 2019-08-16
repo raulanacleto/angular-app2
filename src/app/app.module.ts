e56726efad1eafb7ed7946b6080b96956f1c036a
raulanacleto <raulanacletosilva@gmail.com>
2019-08-16 09:29:47 -0300

    trabalhando com rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'; //HttpModule em versao antiga
 
+import {RouterModule} from '@angular/router'
+import {ROUTES} from './app.routes'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { HomeComponent } from './home/home.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { DiversaoComponent } from './diversao/diversao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
     HomeComponent,
-    RodapeComponent
+    RodapeComponent,
+    RestaurantesComponent,
+    DiversaoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule //HttpModule em versao antiga
+    HttpClientModule, //HttpModule em versao antiga
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
